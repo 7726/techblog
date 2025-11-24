8fc9e4ea22639b9bd09b18214a954f368afbf28e
BINGO <bingo.devops@gmail.com>
2025-11-24 23:10:39 +0900

    Spring Security 설정 회원가입, 로그인 컨트롤러 구축

---
+++ src/main/java/com/jyo/techblog/domain/user/AuthController.java
@@ -0,0 +1,43 @@
+package com.jyo.techblog.domain.user;
+
+import com.jyo.techblog.domain.user.dto.LoginRequest;
+import com.jyo.techblog.domain.user.dto.RegisterRequest;
+import com.jyo.techblog.domain.user.dto.UserResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 인증 관련 API 컨트롤러
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    /**
+     * 회원가입
+     */
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request) {
+        UserResponse response = userService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    /**
+     * 로그인 (JWT 전 단계)
+     */
+    @PostMapping("/login")
+    public ResponseEntity<UserResponse> login(@Valid @RequestBody LoginRequest request) {
+        UserResponse response = userService.login(request);
+        return ResponseEntity.ok(response);
+    }
+
+}
