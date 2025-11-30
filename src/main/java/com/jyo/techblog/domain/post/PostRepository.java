aac16aaf5ee0d4d959c2c6854930035ba546be31
BINGO <bingo.devops@gmail.com>
2025-11-30 23:57:57 +0900

    Post(게시글) 페이징, 최신순 정렬 기능 추가

--- src/main/java/com/jyo/techblog/domain/post/PostRepository.java
+++ src/main/java/com/jyo/techblog/domain/post/PostRepository.java
@@ -1,6 +1,7 @@
 package com.jyo.techblog.domain.post;
 
 import com.jyo.techblog.domain.user.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,7 +15,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Optional<Post> findByIdAndDeletedFalse(Long id);
 
     // 삭제되지 않은 글 목록 전체
-    List<Post> findByDeletedFalse();
+    List<Post> findByDeletedFalse(Pageable pageable);
 
     // 삭제되지 않은 글만 대상 + 제목/내용 검색
     @Query("""
@@ -23,8 +24,8 @@ public interface PostRepository extends JpaRepository<Post, Long> {
         WHERE p.deleted = false
         AND (p.title LIKE %:keyword% OR p.content LIKE %:keyword%)
     """)
-    List<Post> searchByKeyword(@Param("keyword") String keyword);
+    List<Post> searchByKeyword(@Param("keyword") String keyword, Pageable pageable);
 
     // 필요하면 특정 작성자의 글들 검색 (삭제되지 않은 것만)
-    List<Post> findByAuthorAndDeletedFalse(User author);
+    List<Post> findByAuthorAndDeletedFalse(User author, Pageable pageable);
 }
