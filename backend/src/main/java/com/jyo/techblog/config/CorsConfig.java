60b42c9af015f522b3b510c29b2b614f12da34a4
BINGO <bingo.devops@gmail.com>
2025-12-23 23:13:23 +0900

    - FE 주소 변수명 숨기기 작업

--- backend/src/main/java/com/jyo/techblog/config/CorsConfig.java
+++ backend/src/main/java/com/jyo/techblog/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.jyo.techblog.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,13 +10,16 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
+    @Value("${app.cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);  // 자격 증명(쿠키, 인증 헤더 등) 허용
-        config.addAllowedOrigin("http://localhost:3000");  // 프론트엔드 주소만 허용
+        config.addAllowedOrigin(allowedOrigins);  // 특정 주소만 허용 (프론트엔드)
         config.addAllowedHeader("*");  // 모든 헤더 허용
         config.addAllowedMethod("*");  // GET, POST, PUT, DELETE 등 모든 메서드 허용
 
