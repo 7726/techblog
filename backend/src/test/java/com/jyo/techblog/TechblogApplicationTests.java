82caebac6cde3a833663eb09be6efd03d47dfe6e
BINGO <bingo.devops@gmail.com>
2026-01-26 17:18:14 +0900

    fix: 프론트 오류 수정

--- backend/src/test/java/com/jyo/techblog/TechblogApplicationTests.java
+++ backend/src/test/java/com/jyo/techblog/TechblogApplicationTests.java
@@ -1,13 +1,21 @@
 package com.jyo.techblog;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootTest
 class TechblogApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void generatePassword() {
+		// 1. 스프링 도움 없이 직접 생성 (가볍고 빠름)
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
+		// 2. 원하는 비밀번호 입력
+		String rawPassword = "admin1234";
+		String encodedPassword = encoder.encode(rawPassword);
+
+		System.out.println("=========================================");
+		System.out.println("비밀번호 해시값: " + encodedPassword);
+		System.out.println("=========================================");
+	}
 }
